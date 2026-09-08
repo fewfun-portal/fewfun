563bea95042fff2b9548a5ff18ba83a018fb6700
fewfun-portal <nn8778488@gmail.com>
2026-09-08 23:29:43 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/fewfun/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
 })
